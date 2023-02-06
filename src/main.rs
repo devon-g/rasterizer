79f2dfc2e7e1272e399c38d4980d3bd076ef2363
Devon Gardner <dgardner365@gmail.com>
2023-02-06 17:31:32 -0500

    removed test drawing

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::EventPump;
-use std::time::Duration;
 
 /// Produces a [`Canvas`] and an [`EventPump`]
 fn init_sdl(title: &str, width: u32, height: u32) -> (Canvas, EventPump) {
@@ -31,20 +30,7 @@ fn init_sdl(title: &str, width: u32, height: u32) -> (Canvas, EventPump) {
 fn main() {
     let (mut canvas, mut event_pump) = init_sdl("Rust SDL2", 1280, 720);
 
-    let mut x = -canvas.width / 2;
-    let mut y = canvas.height / 2;
     'running: loop {
-        canvas.put_pixel(x, y, Color::RGB(255, 255, 255));
-        x += 1;
-        y -= 1;
-
-        if x >= canvas.width / 2 {
-            x -= canvas.width;
-        }
-        if y < -canvas.height / 2 {
-            y += canvas.height;
-        }
-
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
@@ -55,7 +41,5 @@ fn main() {
                 _ => {}
             }
         }
-
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 144));
     }
 }
