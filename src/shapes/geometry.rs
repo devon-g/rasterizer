3fd8cae5ae966f12b9fdc9f6cfd77ebc8ab1e9a3
Devon Gardner <dgardner365@gmail.com>
2023-03-02 16:08:40 -0500

    Added viewport for projecting 3d scenes onto 2d screen

--- src/shapes/geometry.rs
+++ src/shapes/geometry.rs
@@ -1,13 +1,13 @@
 #[derive(Copy, Clone)]
-pub struct Point {
+pub struct Point2 {
     pub x: f32,
     pub y: f32,
     pub h: f32,
 }
 
-impl Point {
-    pub fn new(x: f32, y: f32, h: f32) -> Self {
-        Self {
+impl Point2 {
+    pub fn new(x: f32, y: f32, h: f32) -> Point2 {
+        Point2 {
             x,
             y,
             h: h.clamp(0.0, 1.0),
@@ -15,9 +15,27 @@ impl Point {
     }
 }
 
-impl Into<sdl2::rect::Point> for Point {
+impl Into<sdl2::rect::Point> for Point2 {
     fn into(self) -> sdl2::rect::Point {
         sdl2::rect::Point::new(self.x as i32, self.y as i32)
+    }
+}
+
+pub struct Point3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub h: f32,
+}
+
+impl Point3 {
+    pub fn new(x: f32, y: f32, z: f32, h: f32) -> Point3 {
+        Point3 {
+            x,
+            y,
+            z,
+            h: h.clamp(0.0, 1.0),
+        }
     }
 }
 
---
+++ src/viewport.rs
@@ -0,0 +1,31 @@
+use crate::canvas::Canvas;
+use crate::shapes::geometry::Point2;
+use crate::shapes::geometry::Point3;
+
+pub struct Viewport {
+    pub cw: f32,
+    pub ch: f32,
+    pub vw: f32,
+    pub vh: f32,
+    pub d: f32,
+}
+
+impl Viewport {
+    pub fn new(width: f32, height: f32, depth: f32, canvas: &Canvas) -> Self {
+        Viewport {
+            cw: canvas.width as f32,
+            ch: canvas.height as f32,
+            vw: width,
+            vh: height,
+            d: depth,
+        }
+    }
+
+    pub fn viewport_to_canvas(&self, p: Point2) -> Point2 {
+        Point2::new(p.x * (self.cw / self.vw), p.y * (self.ch / self.vh), p.h)
+    }
+
+    pub fn project_vertex(&self, v: &Point3) -> Point2 {
+        self.viewport_to_canvas(Point2::new(v.x * self.d / v.z, v.y * self.d / v.z, v.h))
+    }
+}
