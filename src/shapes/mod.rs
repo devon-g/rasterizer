fe1fb003d7ec5897e360279e5c9686124809e3de
Devon Gardner <dgardner365@gmail.com>
2023-03-21 18:47:28 -0400

    Removed rectangle things

--- src/shapes/mod.rs
+++ src/shapes/mod.rs
@@ -1,11 +1,1 @@
 pub mod geometry;
-//pub mod rectangle;
-
-use crate::canvas::Canvas;
-use geometry::Color;
-// use geometry::Point;
-
-pub trait Drawable {
-    fn draw(&self, canvas: &mut Canvas, color: Color);
-    // fn get_points(&self) -> Vec<Point>;
-}
