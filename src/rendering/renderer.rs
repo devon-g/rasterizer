f72557a688e00a67096745eaba0ee2887b7fba73
Devon Gardner <dgardner365@gmail.com>
2023-03-27 15:08:54 -0400

    Combined canvas and viewport into a single rendering object

--- src/rendering/renderer.rs
+++ src/rendering/renderer.rs
@@ -1,7 +1,36 @@
-use crate::rendering::viewport::Viewport;
 use crate::rendering::canvas::Canvas;
+use crate::rendering::viewport::Viewport;
+use crate::shapes::geometry::{Point2, Point3};
+use crate::shapes::triangle::Triangle;
 
 pub struct Renderer {
-    canvas: Canvas,
-    viewport: Viewport
+    pub canvas: Canvas,
+    pub viewport: Viewport,
+}
+
+impl Renderer {
+    pub fn new(canvas: Canvas, viewport: Viewport) -> Renderer {
+        Renderer { canvas, viewport }
+    }
+
+    pub fn render_object(&mut self, vertices: Vec<Point3>, triangles: Vec<Triangle>) {
+        let mut projected: Vec<Point2> = Vec::new();
+        // Convert all 3d points into 2d points
+        for vertex in vertices {
+            projected.push(self.viewport.project_vertex(&vertex));
+        }
+        // Render the triangles
+        for triangle in triangles {
+            self.render_triangle(triangle, &projected);
+        }
+    }
+
+    pub fn render_triangle(&mut self, triangle: Triangle, projected: &Vec<Point2>) {
+        self.canvas.draw_wireframe_triangle(
+            projected[triangle.vertices[0] as usize],
+            projected[triangle.vertices[1] as usize],
+            projected[triangle.vertices[2] as usize],
+            triangle.color
+        );
+    }
 }
