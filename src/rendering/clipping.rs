af9ae322bcf3ed8195056af327a4c9e4569b607d
Devon Gardner <dgardner365@gmail.com>
2023-04-29 22:49:24 -0400

    changed some things

--- src/rendering/clipping.rs
+++ src/rendering/clipping.rs
@@ -1,19 +1,20 @@
 use super::viewport::Plane;
-use crate::{color, models::triangle::Triangle};
+use crate::models::triangle::Triangle;
+use nalgebra_glm::Vec4;
 
-pub fn clip_triangle(triangle: Triangle, plane: Plane) -> &[Triangle] {
+pub fn clip_triangle(triangle: Triangle, plane: Plane) -> Vec<Triangle> {
     let d0 = plane.signed_distance(triangle.vertices[0]);
     let d1 = plane.signed_distance(triangle.vertices[1]);
     let d2 = plane.signed_distance(triangle.vertices[2]);
 
     if d0.is_sign_positive() && d1.is_sign_positive() && d2.is_sign_positive() {
-        return &[triangle];
+        return [triangle].to_vec();
     } else if d0.is_sign_negative() && d1.is_sign_negative() && d2.is_sign_negative() {
-        return &[];
+        return [].to_vec();
     } else if d0.is_sign_positive() ^ d1.is_sign_positive() ^ d2.is_sign_positive() {
-        let mut a: Vec4;
-        let mut b: Vec4;
-        let mut c: Vec4;
+        let mut a: i32;
+        let mut b: i32;
+        let mut c: i32;
         if d0.is_sign_positive() {
             a = triangle.vertices[0];
             b = triangle.vertices[1];
@@ -30,5 +31,7 @@ pub fn clip_triangle(triangle: Triangle, plane: Plane) -> &[Triangle] {
         let bp = plane.intersection(a, b);
         let cp = plane.intersection(a, c);
         todo!("Need triangle object to contain vertices");
+    } else {
+        ..
     }
 }
